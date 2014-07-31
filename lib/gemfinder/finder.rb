200888351b4d8b7f634b08c62c84501498495836
Chelsea <chelsea.dommert@gmail.com>
2014-07-30 20:55:31 -0500

    Test phrases

--- lib/gemfinder/finder.rb
+++ lib/gemfinder/finder.rb
@@ -5,6 +5,8 @@ module Gemfinder
 
       def gemfind(search_terms)
 
+        puts Gems.info(search_terms)
+
         matches = Gems.search(search_terms)
         matches.each do |candidate_gem|
           puts "#{candidate_gem}: "
