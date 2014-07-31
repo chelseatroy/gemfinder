15b1d518df2fe58ccf5b87c7aeb137b0bcedf8e1
Chelsea <chelsea.dommert@gmail.com>
2014-07-30 21:07:47 -0500

    Test method

--- lib/gemfinder/finder.rb
+++ lib/gemfinder/finder.rb
@@ -4,15 +4,19 @@ require "gems"
 module Gemfinder
   class Finder
 
-      def gemfind(search_terms)
+      def gemfind(search_term)
 
-        puts Gems.info(search_terms)
+        info = Gems.info search_term
+        puts info["name"]
+        puts info["version"]
+        puts info["info"]
 
-        matches = Gems.search(search_terms)
-        matches.each do |candidate_gem|
-          puts "#{candidate_gem}: "
-          puts Gems.info(candidate_gem)
-        end
+
+        # matches = Gems.search(search_terms)
+        # matches.each do |candidate_gem|
+        #   puts "#{candidate_gem}: "
+        #   puts Gems.info(candidate_gem)
+        # end
       end
 
     end
