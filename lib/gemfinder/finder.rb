0b9a918fc698b942d4fec822feec18bcc18988c2
Chelsea <chelsea.dommert@gmail.com>
2014-07-31 15:03:54 -0500

    Test search feature

--- lib/gemfinder/finder.rb
+++ lib/gemfinder/finder.rb
@@ -6,17 +6,17 @@ module Gemfinder
 
       def gemfind search_term
 
-        info = Gems.info search_term
+        # info = Gems.info search_term
        
-        puts info["name"]
-        puts info["version"]
-        puts info["info"]
+        # puts info["name"]
+        # puts info["version"]
+        # puts info["info"]
 
 
-        # matches = Gems.search(search_terms)
-        # matches.each do |candidate_gem|
-        #   puts "#{candidate_gem}: "
-        #   puts Gems.info(candidate_gem)
+        matches = Gems.search search_term
+        matches.each do |candidate_gem|
+          jaim = Gems.info candidate_gem
+          puts jaim["name"]
         end
       end
 
