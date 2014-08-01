93edc4e532052a2d1cf2969b8430cad6d296523c
Chelsea <chelsea.dommert@gmail.com>
2014-07-31 19:16:59 -0500

    Adds catches for invalid searches

--- lib/gemfinder/finder.rb
+++ lib/gemfinder/finder.rb
@@ -6,21 +6,30 @@ module Gemfinder
 
       def gemfind search_term
         info = Gems.info search_term
-       
-        puts info["name"]
-        puts info["version"]
-        puts info["info"]
-        puts info["project_uri"]
+      
+        if info["name"]
+          puts info["name"]
+          puts info["version"]
+          puts info["info"]
+          puts info["project_uri"]
+        else
+          puts "Sorry: we couldn't find a gem by that name."
+        end
+
       end
 
       def gem_explore search_term
         matches = Gems.search search_term
 
-        matches.each do |candidate_gem|
-          puts candidate_gem["name"]
-          puts candidate_gem["info"]
-          puts candidate_gem["project_uri"]
-          puts
+        if matches[0]
+          matches.each do |candidate_gem|
+            puts candidate_gem["name"]
+            puts candidate_gem["info"]
+            puts candidate_gem["project_uri"]
+            puts
+          end
+        else
+          puts "SorryL we couldn't find any gems with that search term in the name."
         end
       end
 
